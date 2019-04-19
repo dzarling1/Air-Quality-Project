b6408ae8ef8d5a219d4c213039384e4cfade8729
mohannedsa <abuh4692@stthomas.edu>
2019-04-19 13:20:44 -0500

    ...

--- data_02/my_map/lib/mapping.js
+++ data_02/my_map/lib/mapping.js
@@ -82,13 +82,14 @@ function onMapMove() {
             var i;
             var lats;
             var logs;
-            for (i = 0; i < 10; i++) {
+            for (i = 0; i <Object.keys(data2).length; i++) {
                 //console.log("Locations: " +data2.results[i].location);
                 lats = data2.results[i].coordinates.latitude;
                 logs = data2.results[i].coordinates.longitude;
                 //console.log("cords: " +lats + " l " + logs);
                 var markers = L.marker([lats, logs]).bindPopup("Location: "+data2.results[i].location +"<br>"+ "Parameter: " +data2.results[i].parameter).addTo(map);
                 map.addLayer(markers);
+
                 var firstTable = new Vue({
                     el: '#firstTable',
                     data: {
@@ -98,12 +99,6 @@ function onMapMove() {
                     }
                 });
             }
-            markers.on('mouseover', function(event){
-                markers.openPopup();
-                });
-                markers.on('mouseout', function(event){
-                markers.closePopup();
-                });
         }
     };
     xhttp.open("GET", "https://api.openaq.org/v1/measurements?limit=100&coordinates=" + document.getElementById("lat").value + "," + document.getElementById("lon").value + "&radius=" + radius, true);
